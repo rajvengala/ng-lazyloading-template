394f0a623ee9b28fb25071476093adbb290bd362
Raj Vengala <raj.vengala@colortokens.com>
2018-04-01 10:32:20 +0530

    added 2nd comp in module1

--- src/app/modules/module1/module1-routing.module.ts
+++ src/app/modules/module1/module1-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from '../../app.component';
 import {Component1Component} from './components/component1/component1.component';
+import {Component4Component} from './components/component4/component4.component';
 
 const routes: Routes = [
   {path: '', component: Component1Component},
+  {path: 'comp4', component: Component4Component}
 ];
 
 @NgModule({
--- src/app/modules/module1/module1.module.ts
+++ src/app/modules/module1/module1.module.ts
@@ -5,6 +5,7 @@ import {Module1RoutingModule} from './module1-routing.module';
 import {Component1Component} from './components/component1/component1.component';
 import {CoreModule} from '../core/core.module';
 import {Service1Service} from './services/service1.service';
+import { Component4Component } from './components/component4/component4.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,7 @@ import {Service1Service} from './services/service1.service';
     // which in turn makes exported components from CoreModule accessible in this module
     CoreModule
   ],
-  declarations: [Component1Component],
+  declarations: [Component1Component, Component4Component],
   providers: [
     // Services in feature modules should be used outside this module
     Service1Service
